82047b26d60da3cd5f149ff4071ff78a9fa5060d
Serhii Pimenov <serhii@pimenov.com.ua>
2025-05-13 17:56:27 +0300

    upd icon for action

--- src/main/kotlin/ua/com/pimenov/latte/utils/icons.kt
+++ src/main/kotlin/ua/com/pimenov/latte/utils/icons.kt
@@ -6,8 +6,4 @@ import javax.swing.Icon
 object LatteIcons {
     @JvmField
     val LatteIcon: Icon = IconLoader.getIcon("/icons/latte.svg", LatteIcons::class.java)
-
-    // Можете додати інші іконки, якщо потрібно
-//    @JvmField
-//    val RunAll: Icon = IconLoader.getIcon("/icons/run_all.svg", LatteIcons::class.java)
 }
